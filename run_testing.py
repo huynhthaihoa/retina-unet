fd17328a86bb8528d62d8c3049deb6c484c44648
Hoa <huynhlequang@gmail.com>
2019-06-19 00:44:59 +0900

    Add many things

--- run_testing.py
+++ run_testing.py
@@ -20,7 +20,7 @@ run_GPU = '' if sys.platform == 'win32' else ' THEANO_FLAGS=device=gpu,floatX=fl
 
 #create a folder for the results if not existing already
 result_dir = name_experiment
-print "\n1. Create directory for the results (if not already existing)"
+print("\n1. Create directory for the results (if not already existing)")
 if os.path.exists(result_dir):
     pass
 elif sys.platform=='win32':
@@ -31,8 +31,8 @@ else:
 
 # finally run the prediction
 if nohup:
-    print "\n2. Run the prediction on GPU  with nohup"
+    print("\n2. Run the prediction on GPU  with nohup")
     os.system(run_GPU +' nohup python -u ./src/retinaNN_predict.py > ' +'./'+name_experiment+'/'+name_experiment+'_prediction.nohup')
 else:
-    print "\n2. Run the prediction on GPU (no nohup)"
+    print("\n2. Run the prediction on GPU (no nohup)")
     os.system(run_GPU +' python ./src/retinaNN_predict.py')
